a15a493ed6a75f2a560950138e12e8b26bea2cac
serhatbek <serhat.bekk@gmail.com>
2023-10-31 14:19:21 +0300

    setting up home slice

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import homeReducer from './homeSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    homeReducer,
+  },
 });
