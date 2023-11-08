9bda1d93925c193a9296ce20ed082ac4968496f7
serhatbek <serhat.bekk@gmail.com>
2023-11-08 11:36:21 +0300

    Carousel Section - Sliding Functionality

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -21,7 +21,20 @@ const Carousel = ({ data, loading }) => {
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
 
-  const navigation = (dir) => {};
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+
+    const scrollAmount =
+      dir === 'left'
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: 'smooth',
+    });
+  };
+
   const skItem = () => {
     return (
       <div className='skeletonItem'>
@@ -38,7 +51,7 @@ const Carousel = ({ data, loading }) => {
     <div className='carousel'>
       <ContentWrapper>
         <BsFillArrowLeftCircleFill
-          className='carouselLefNav arrow'
+          className='carouselLeftNav arrow'
           onClick={() => navigation('left')}
         />
         <BsFillArrowRightCircleFill
@@ -46,13 +59,17 @@ const Carousel = ({ data, loading }) => {
           onClick={() => navigation('right')}
         />
         {!loading ? (
-          <div className='carouselItems'>
+          <div className='carouselItems' ref={carouselContainer}>
             {data?.map((item) => {
               const posterUrl = item.poster_path
                 ? url.poster + item.poster_path
                 : PosterFallback;
               return (
-                <div key={item.id} className='carouselItem'>
+                <div
+                  key={item.id}
+                  className='carouselItem'
+                  onClick={() => navigate(`/${item.media_type}/${item.id}`)}
+                >
                   <div className='posterBlock'>
                     <ImageLazyLoad src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />
